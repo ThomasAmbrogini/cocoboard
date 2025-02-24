0f6255f53379d989345d56543358b210ba448a53
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2025-02-24 22:40:33 +0100

    temp: starting project

---
+++ cmake/stm32f411.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+#TODO: specify the arguments to compile for the stm32f411 (only the mandatory ones)
+#TODO: is freestanding argument needed?
+set(CMAKE_C_COMPILER  /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++)
+
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT -nostdlib)
+
+#TODO: there are different things to do in order to enable C++ to be used like this.
+
