c96418436d904ba0115e2948c63ecbc3a399ecd4
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2025-02-25 22:20:14 +0100

    temp: continuing starting

--- main.cpp
+++ main.cpp
@@ -1,24 +1,5 @@
-class Obj
-{
-public:
-    Obj() : a(1) {}
-    Obj(int c) : a(c) {}
-    ~Obj() {}
-private:
-    int a;
-};
-
-struct Str
-{
-    int b;
-};
-
-Str g_str{10};
-Obj g_a();
-
 int main() {
     int a = 4;
-    Obj o(a);
 
     a = 56;
 }
