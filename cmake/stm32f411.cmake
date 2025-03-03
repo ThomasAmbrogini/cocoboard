7604ef30a660cb7311ee2ef03b58bcd347e25a8e
Thomas Ambrogini <thomas.ambrogini@gmail.com>
2025-03-03 22:09:34 +0100

    temp: add some stuff to start tinkering

--- cmake/stm32f411.cmake
+++ cmake/stm32f411.cmake
@@ -2,8 +2,8 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 #TODO: specify the arguments to compile for the stm32f411 (only the mandatory ones)
-set(CMAKE_C_COMPILER  /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++)
+set(CMAKE_C_COMPILER  /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc -march=armv7+fp -mtune=cortex-m4)
+set(CMAKE_CXX_COMPILER /opt/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ -march=armv7+fp -mtune=cortex-m4)
 
 set(CMAKE_C_FLAGS_INIT -ffreestanding)
 set(CMAKE_CXX_FLAGS_INIT -ffreestanding)
--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+static void startup_handler();
+
+using Isr = void (*)();
+
+Isr interrupt_vector_table[] = {
+    0,
+    startup_handler
+};
+
+void startup_handler() {
+    int c = 4 + 5;
+}
+
 int main() {
     int a = 4;
 
